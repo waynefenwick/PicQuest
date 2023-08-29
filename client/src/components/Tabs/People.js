c821745ad71db9975a12f8447b55cdf838b422a4
Wayne <waynefenwick@gmail.com>
2023-08-28 21:06:01 -0500

    Improved stying of homepage mobile scroll bar; added a star rating, thumbsup and download option for pictures...only applied to People.js for now

--- client/src/components/Tabs/People.js
+++ client/src/components/Tabs/People.js
@@ -1,36 +1,62 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faThumbsUp, faDownload, faStar } from '@fortawesome/free-solid-svg-icons';
 import peopleImage1 from '../../Media/People/people.png';
 import peopleImage2 from '../../Media/People/cyclists.png';
-import peopleImage3 from '../../Media/People/cyclists.png';
+// ... other imports
 
-
+const imagePaths = [peopleImage1, peopleImage2,]; // Add more paths if needed
 
 const People = () => {
+  const imagePaths = [peopleImage1, peopleImage2]; // Add more paths if needed
+
+  const [ratings, setRatings] = useState(Array(imagePaths.length).fill(null));
+
+  const handleDownload = (imagePath) => {
+    const link = document.createElement('a');
+    link.href = imagePath;
+    link.download = `image${imagePaths.indexOf(imagePath) + 1}.png`;
+    link.click();
+  };
+
+  const handleStarClick = (index, selectedRating) => {
+    const newRatings = [...ratings];
+    newRatings[index] = selectedRating;
+    setRatings(newRatings);
+  };
+
   return (
     <div id="people" className="section">
-      <div className="box">
-        <img src={peopleImage1} className="photo" alt="Image 1" />
-        <a href={peopleImage1} download className="download-option">
-          Download
-        </a>
-      </div>
-      <div className="box">
-        <img src={peopleImage2} className="photo" alt="Image 2" />
-        <a href={peopleImage2} download className="download-option">
-          Download
-        </a>
-      </div>
-      <div className="box">
-        <img src={peopleImage3} className="photo" alt="Image 3" />
-        <a href={peopleImage3} download className="download-option">
-          Download
-        </a>
-      </div>
-      {/* ... Other boxes */}
+      {imagePaths.map((imagePath, index) => (
+        <div className="box" key={index}>
+          <div className="image-container">
+            <img src={imagePath} className="photo" alt={`Image ${index + 1}`} />
+            <div className="overlay">
+              <FontAwesomeIcon icon={faThumbsUp} className="icon thumbs-up" />
+              <FontAwesomeIcon
+                icon={faDownload}
+                className="icon download"
+                onClick={() => handleDownload(imagePath)}
+              />
+              <div className="star-rating">
+                {[1, 2, 3, 4, 5].map((star) => (
+                  <FontAwesomeIcon
+                    key={star}
+                    icon={faStar}
+                    className={`icon star ${star <= ratings[index] ? 'filled' : ''}`}
+                    onClick={() => handleStarClick(index, star)}
+                  />
+                ))}
+              </div>
+            </div>
+          </div>
+        </div>
+      ))}
     </div>
   );
 };
 
+
+
+
 export default People;
-
-
